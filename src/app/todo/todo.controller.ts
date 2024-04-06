4d0a8b6ce3d77df5f9e48d794d9b4301694d8466
Victor Ozoterio <victorozoterio@gmail.com>
2024-04-06 17:47:11 -0300

    Documentação finalizada

--- src/app/todo/todo.controller.ts
+++ src/app/todo/todo.controller.ts
@@ -3,6 +3,12 @@ import { TodoService } from './todo.service';
 import { CreateTodoDto } from './create-todo.dto';
 import { UpdateTodoDto } from './update-todo.dto';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { IndexTodoSwagger } from './swagger/index-todo.swagger';
+import { CreateTodoSwagger } from './swagger/create-todo.swagger';
+import { ShowTodoSwagger } from './swagger/show-todo.swagger';
+import { UpdateTodoSwagger } from './swagger/update-todo.swagger';
+import { BadRequestSwagger } from 'src/helpers/swagger/bad-request.swagger';
+import { NotfoundSwagger } from 'src/helpers/swagger/not-found.swagger';
 
 @Controller('api/v1/todos')
 @ApiTags('todos')
@@ -11,7 +17,12 @@ export class TodoController {
 
     @Get()
     @ApiOperation({ summary: 'Listar todas as tarefas' })
-    @ApiResponse({ status: 200, description: 'Lista de tarefas retornada com sucesso' })
+    @ApiResponse({ 
+        status: 200, 
+        description: 'Lista de tarefas retornada com sucesso',
+        type: IndexTodoSwagger,
+        isArray: true,
+     })
     
     async index() {
         return await this.todoService.findAll();
@@ -19,24 +30,49 @@ export class TodoController {
 
     @Post()
     @ApiOperation({ summary: 'Adicionar um nova tarefa' })
-    @ApiResponse({ status: 201, description: 'Nova tarefa criada com sucesso' })
-    @ApiResponse({ status: 400, description: 'Parâmetros inválidos' })
+    @ApiResponse({ status: 201, description: 'Nova tarefa criada com sucesso', type: CreateTodoSwagger })
+    @ApiResponse({ 
+        status: 400, 
+        description: 'Parâmetros inválidos', 
+        type: BadRequestSwagger,
+    })
     async create(@Body() body: CreateTodoDto) {
         return await this.todoService.create(body);
     }
 
     @Get(':id')
     @ApiOperation({ summary: 'Exibir os dados de uma tarefa' })
-    @ApiResponse({ status: 200, description: 'Dados de uma tarefa retornado com sucesso' })
-    @ApiResponse({ status: 404, description: 'Task não foi encontrada' })
+    @ApiResponse({ 
+        status: 200, 
+        description: 'Dados de uma tarefa retornado com sucesso',
+        type: ShowTodoSwagger,
+     })
+    @ApiResponse({
+        status: 404,
+        description: 'Task não foi encontrada',
+        type: NotfoundSwagger,
+        
+    })
     async show(@Param('id', new ParseUUIDPipe()) id: string) {
     return await this.todoService.findOneOrFail(id);
     }
 
     @Put(':id')
     @ApiOperation({ summary: 'Atualizar os dados de uma tarefa' })
-    @ApiResponse({ status: 200, description: 'Tarefa atualizada com sucesso' })
-    @ApiResponse({ status: 404, description: 'Task não foi encontrada' })
+    @ApiResponse({ 
+        status: 200, 
+        description: 'Tarefa atualizada com sucesso',
+        type: UpdateTodoSwagger,
+    })
+    @ApiResponse({status:400,
+        description: 'Dados inválidos',
+        type: BadRequestSwagger,
+    })
+    @ApiResponse({ 
+        status: 404,
+        description: 'Task não foi encontrada',
+        type: NotfoundSwagger,
+    })
     async update(@Param('id', new ParseUUIDPipe())  id: string, 
     @Body() body: UpdateTodoDto) {
     return await this.todoService.update(id, body);
@@ -45,7 +81,11 @@ export class TodoController {
     @Delete(':id')
     @ApiOperation({ summary: 'Remover uma tarefa' })
     @ApiResponse({ status: 204, description: 'Tarefa removida com sucesso' })
-    @ApiResponse({ status: 404, description: 'Task não foi encontrada' })
+    @ApiResponse({
+        status: 404,
+        description: 'Task não foi encontrada',
+        type: NotfoundSwagger,
+    })
     @HttpCode(HttpStatus.NO_CONTENT)
     async destroy(@Param('id', new ParseUUIDPipe()) id: string ) {
         await this.todoService.deleteById(id);
