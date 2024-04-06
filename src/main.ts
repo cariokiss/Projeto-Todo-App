805a7d9e475c5b9017b17665bfc6363015b6da25
Victor Ozoterio <victorozoterio@gmail.com>
2024-04-06 15:42:15 -0300

    API documentada

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder } from '@nestjs/swagger/dist/document-builder';
+import { SwaggerModule } from '@nestjs/swagger/dist/swagger-module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('TODOApp API')
+    .setVersion('0.0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  
+  SwaggerModule.setup('swagger', app, document);
+
   app.useGlobalPipes(new ValidationPipe())
   await app.listen(3000);
 }
